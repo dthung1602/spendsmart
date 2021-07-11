42a8b7d01e954f806cac0a9b2d9fc2610aef460f
dthung1602 <dthung1602@gmail.com>
2021-07-11 12:56:35 +0700

    Add category filter

--- src/parts/TransactionFilter/TransactionFilter.tsx
+++ src/parts/TransactionFilter/TransactionFilter.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEventHandler } from "react";
-import { cloneDeep, get, set } from "lodash";
+import React, { ChangeEventHandler, useContext } from "react";
+import { cloneDeep, get, set, omit } from "lodash";
 
 import { Affix } from "../../components";
+import { GlobalContext } from "../../GlobalContext";
 import { QueryObject, Transaction } from "../../database";
 import type { ReactSetter } from "../../utils/types";
 import "./TransactionFilter.less";
@@ -15,22 +16,35 @@ interface TransactionFiltersProps {
 
 const strToDate = (str: string) => new Date(str);
 
+const EMPTY = "-----";
+
 function TransactionFilter({
   filter,
   setFilter,
 }: TransactionFiltersProps): JSX.Element {
+  const { categoryOptions } = useContext(GlobalContext);
+
   const dateGTE = filter.spendDatetime?.$gte?.toISOString().slice(0, 10) || "";
   const dateLTE = filter.spendDatetime?.$lte?.toISOString().slice(0, 10) || "";
+
+  const categoriesTitles = filter.categoriesTitles?.$eq;
 
   function onChange<T>(
     fieldPath: string,
     cast?: (value: string) => T
-  ): ChangeEventHandler<HTMLInputElement> {
+  ): ChangeEventHandler<HTMLInputElement | HTMLSelectElement> {
     return (event) => {
       const value = cast ? cast(event.target.value) : event.target.value;
       if (get(filter, fieldPath) !== value) {
-        const newFilter = cloneDeep(filter);
-        set(newFilter, fieldPath, value);
+        let newFilter = cloneDeep(filter);
+        if (value === EMPTY) {
+          newFilter = omit(
+            newFilter,
+            fieldPath.split(".").slice(0, -1).join(".")
+          );
+        } else {
+          set(newFilter, fieldPath, value);
+        }
         setFilter(newFilter);
       }
     };
@@ -50,6 +64,19 @@ function TransactionFilter({
         onChange={onChange("spendDatetime.$lte", strToDate)}
         placeholder="To"
       />
+      <select
+        value={categoriesTitles}
+        onChange={onChange("categoriesTitles.$eq")}
+      >
+        <option key={EMPTY} value={EMPTY}>
+          {EMPTY}
+        </option>
+        {categoryOptions.map((opt) => (
+          <option key={opt.value} value={opt.displayText as string}>
+            {opt.displayText}
+          </option>
+        ))}
+      </select>
     </Affix>
   );
 }
